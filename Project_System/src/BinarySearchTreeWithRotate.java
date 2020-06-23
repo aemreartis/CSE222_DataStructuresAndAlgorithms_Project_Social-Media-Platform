c6621779686118e09dd695f60fbc6484e1ca6ece
Ahmet Eren Incir <33928836+AhmetErenIncir@users.noreply.github.com>
2020-06-23 23:45:14 +0300

    Add files via upload

---
+++ Project_System/src/BinarySearchTreeWithRotate.java
@@ -0,0 +1,42 @@
+@SuppressWarnings("serial")
+public class BinarySearchTreeWithRotate<E extends Comparable<E>>
+        extends BinarySearchTree<E> {
+
+    //Methods
+
+    /**
+     * Method to perform a right rotation
+     * pre: root is the root of a binary search tree
+     * post: root.left is the root of a binary search tree
+     * 		 root.left.left is raised one level
+     * 		 root.left.right does not change levels
+     * 		 root.right is lowered one level
+     * 		 the new root is returned
+     * @param root The root of the binary tree to be rotated
+     * @return The new root of the rotated tree
+     */
+    protected Node<E> rotateRight(Node<E> root){
+        Node<E> temp = root.left;
+        root.left = temp.right;
+        temp.right = root;
+        return temp;
+    }
+
+    /**
+     * Method to perform a left rotation
+     * pre: root is the root of a binary search tree
+     * post: root.right is the root of a binary search tree
+     * 		 root.right.right is raised one level
+     * 		 root.right.left does not change levels
+     * 		 root.left is lowered one level
+     * 		 the new root is returned
+     * @param root The root of the binary tree to be rotated
+     * @return The new root of the rotated tree
+     */
+    protected Node<E> rotateLeft(Node<E> root){
+        Node<E> temp = root.right;
+        root.right = temp.left;
+        temp.left = root;
+        return temp;
+    }
+}
