326c2b2a2385db3b8811927b27ef9c9979dc78a9
Emre <emreartis1996@gmail.com>
2020-06-23 14:08:05 +0300

    Update Time.java

--- Project_System/src/Time.java
+++ Project_System/src/Time.java
@@ -8,4 +8,28 @@ public class Time {
         this.minutes = minutes;
         this.second = second;
     }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
 }
