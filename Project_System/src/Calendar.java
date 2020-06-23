39f1fa4d27de1d38e87a954d3913b0737fb3ab87
Emre <emreartis1996@gmail.com>
2020-06-23 14:07:37 +0300

    Update Calendar.java

--- Project_System/src/Calendar.java
+++ Project_System/src/Calendar.java
@@ -1,4 +1,20 @@
 public class Calendar {
-    Date date,
+    Date date;
     Time time;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
 }
