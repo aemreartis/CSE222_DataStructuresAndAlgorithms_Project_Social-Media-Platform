0bdbafd737c8d94c7d90354a7dc2b3eaf6b4c5fe
TugayIpek <58231036+TugayIpek@users.noreply.github.com>
2020-07-08 03:16:46 +0300

    Update Admin.java

--- Project_System/src/Admin.java
+++ Project_System/src/Admin.java
@@ -1,22 +1,30 @@
 public class Admin {
 
+    protected System access;
     protected String name;
     protected String id;
     protected String password;
-    protected String contact;
+    protected String mail;
 
-    public Admin(String name, String id, String password, String contact) {
+    public Admin( String id, String name, String password, String mail) {
         this.name = name;
         this.id = id;
         this.password = password;
-        this.contact = contact;
+        this.mail = mail;
     }
 
-    public boolean addAnnouncements() {
-        return false;
+    public boolean addAnnouncements(String tittle, String announcements,String date) {
+        access.announcements.add(new Announcement(tittle,announcements,date));
+        return true;
     }
 
-    public boolean removeRegisteredUser() {
+    public boolean removeRegisteredUser(String id) {
+        for (int i = 0; i < access.registeredUser.size();i++){
+            if (access.registeredUser.get(i).id.equals(id)){
+                access.registeredUser.remove(i);
+                return true;
+            }
+        }
         return false;
     }
 }
